4fb2e4ac5c8448cefb41d709207a84fb392852f7
ahmed-1430 <ahmedtexting@gmail.com>
2025-11-25 19:00:21 +0600

    authprovider added to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/Component/Navbar";
 import "./globals.css";
 import Footer from "@/Component/Footer";
 import { CartProvider } from "@/Context/CartContext";
+import { AuthProvider } from "@/Context/AuthContext";
 
 export const metadata = {
   title: "PrimeKart",
@@ -12,11 +13,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <CartProvider>
-          <Navbar />
-          {children}
-          <Footer />
-        </CartProvider>
+        <AuthProvider>
+          <CartProvider>
+            <Navbar />
+            {children}
+            <Footer />
+          </CartProvider>
+        </AuthProvider>
+
 
       </body>
     </html>
